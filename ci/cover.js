22f040d03419d84532a869d9771d32203040809b
Taka Okunishi <okunishinishi@gmail.com>
2015-08-07 01:42:54 +0900

    Modify ci scripts

--- ci/cover.js
+++ ci/cover.js
@@ -6,13 +6,22 @@
 
 "use strict";
 
-var path = require('path');
+var path = require('path'),
+    async = require('async'),
+    apeCovering = require('ape-covering');
 
 var basedir = path.resolve(__dirname, '..');
 process.chdir(basedir);
 
-require('istanbul/lib/cli').runToCompletion([
-    'cover',
-    require.resolve('./test.js'),
-    '--dir', 'coverage'
-]);
+async.series([
+    function (callback) {
+        var test = require.resolve('./test');
+        apeCovering.measureCoverage(test, [], {
+            dir: 'coverage'
+        }, callback);
+    }
+], function (err) {
+    if (err) {
+        console.error(err);
+    }
+});
