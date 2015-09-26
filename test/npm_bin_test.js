ceac95759d76f95f8b9bf6ef293c54abc72567f8
okunishinishi <okunishitaka.com@gmail.com>
2015-09-26 13:18:11 +0900

    Version incremented to 2.3.1

---
+++ test/npm_bin_test.js
@@ -0,0 +1,20 @@
+/**
+ * Test case for npmBin.
+ * Runs with nodeunit.
+ */
+
+var npmBin = require('../lib/npm_bin.js');
+
+exports.setUp = function(done) {
+    done();
+};
+
+exports.tearDown = function(done) {
+    done();
+};
+
+exports['Npm bin'] = function(test){
+
+    test.done();
+};
+
